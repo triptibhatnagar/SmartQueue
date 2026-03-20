649be8de73baa52c2f1c2cae81e78799e2f21ccc
triptibhatnagar <bhatnagartripti2727@gmail.com>
2026-03-20 16:58:12 +0530

    added @  scheduled time

--- src/main/java/com/hey/doc/DocApplication.java
+++ src/main/java/com/hey/doc/DocApplication.java
@@ -2,8 +2,10 @@ package com.hey.doc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DocApplication {
 
 	public static void main(String[] args) {
